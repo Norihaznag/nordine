24c791e09bd642dcb56c470325e3a28a55c86233
Noureddine Azinag <127265394+Norihaznag@users.noreply.github.com>
2024-10-27 15:05:44 +0100

    done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a211cdd6bd404ce9"; }
+/******/ 		__webpack_require__.h = function() { return "7ad4c06005bcf166"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.e90043f7d0878a28.hot-update.js
+++ .next/static/webpack/webpack.e90043f7d0878a28.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "32ebe61faafec8e5"; }
+/******/ 	__webpack_require__.h = function() { return "f4dd48104a32c57e"; }
 /******/ }();
 /******/ 
 /******/ }
